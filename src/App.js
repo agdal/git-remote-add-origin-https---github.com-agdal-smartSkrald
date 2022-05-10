a666c358196e26fe3b861c65f06a85505aa7ceda
Oliver Agdal <53648573+agdal@users.noreply.github.com>
2022-05-10 16:59:21 +0200

    updated comments

--- src/App.js
+++ src/App.js
@@ -7,45 +7,48 @@ import '@tomtom-international/web-sdk-maps/dist/maps.css';
 const App = () => {
 
   // Definer nogle ting der skal bruges til mappet
-  const mapElement = useRef()
-  const [map, setMap] = useState({})
+  const mapElement = useRef() // som kaldes længere nede i return statement
+  const [map, setMap] = useState({}) // så vi har et map, men kan også sætte mappet til et nyt 
 
-  const [longitude, setLongitude] = useState(12.6002)
-  const [latitude, setLatitude] = useState(55.631)
+  const [longitude, setLongitude] = useState(12.6002) // hvor vi er longitude
+  const [latitude, setLatitude] = useState(55.631) // hvor vi er latitude
 
 
+  // lav vores positioner om til typen lng lat så vi kan bruge dem som geodata når ruten dannes
   const convertToPoints = (lngLat) => {
     return {
       point: {
-        latitude: lngLat.lat,
-        longitude: lngLat.lng
+        latitude: lngLat.lat, // tager lat på tidspunktet og konvertere til data der kan bruges i mappet
+        longitude: lngLat.lng // samme med lng
       }
     }
   }
 
+  // tegn ruten på mappet
   const drawRoute = (geoJson, map) => {
-    if (map.getLayer('route')) {
-      map.removeLayer('route')
+    if (map.getLayer('route')) { // hvis der allerde er en rute 
+      map.removeLayer('route') // fjern gamle rute
       map.removeSource('route')
     }
-    map.addLayer({
+    map.addLayer({ // lav en ny rute
       id: 'route',
       type: 'line',
       source: {
         type: 'geojson',
         data: geoJson
       },
-      paint: {
+      paint: { // lav rutens farve så vi kan se den
         'line-color': '#283618',
         'line-width': 6
       }
     })
   }
 
+  // lav en ny hente lokation
   const nySkraldHentLokation = (lngLat, map) => {
     const element = document.createElement('div')
     element.className = 'skrald-hent'
-    new tt.Marker({
+    new tt.Marker({ 
       element: element
     })
     .setLngLat(lngLat)
@@ -53,78 +56,94 @@ const App = () => {
   }
 
   // Lav map 
+  // Vi bruger tomtom api til at lave et map med deres gps data
   useEffect(() => {
+    // her er vi:
     const origin = {
-      lng: longitude,
+      lng: longitude, 
       lat: latitude,
     }
 
+    // et array over destinationer på mappet, starter self. med ingen
     const destinations = []
 
+    // laver et map med tomtom api
     let map = tt.map({
       key: "6r7urAO3rpFC6j9sPheFAv0NYswcxfaQ",
       container: mapElement.current,
       center: [longitude, latitude],
       zoom: 14,
-      stylesVisibility: {
+      stylesVisibility: { // Gør vi så vi kan se trafik ulykker og generalt trafik på mappet
         trafficIncidents: true,
         trafficFlow: true
       }
     })
 
-    setMap(map)
+    setMap(map) // sætter mappet til vores map
 
+
+    // vis hvor skraldemanden er
     const nyPrik = () => {
-      
+      // først viser vi hvor skraldemanden er
       const popOffset = {
         bottom: [0,-25]
-      }
+      } // laver en popup der siger hvor man er
       const popup = new tt.Popup({ offset: popOffset}).setHTML('Du er her')
       
       const element = document.createElement('div')
-      element.className = 'prik'
-      const prik = new tt.Marker({
-        draggable: true,
+      element.className = 'prik' // laver en prik som bliver til en skraldbil
+      const prik = new tt.Marker({ // bruger tomtomapi til at laver et marker
+        draggable: true, // gør så skraldebilen kan ændre lokation
         element: element,
       })
-      .setLngLat([longitude, latitude])
-      .addTo(map)
+      .setLngLat([longitude, latitude]) // sætter skraldebilens position
+      .addTo(map) // tilføjer den til mappet
 
-    prik.on('dragend', () => {
-      const lngLat = prik.getLngLat()
-      setLongitude(lngLat.lng)
-      setLatitude(lngLat.lat)
+    prik.on('dragend', () => { // når man trækker i skraldebilen:
+      const lngLat = prik.getLngLat() // find ny lokation
+      setLongitude(lngLat.lng) 
+      setLatitude(lngLat.lat) // sæt skraldebilens ny lokation
     })
 
-    prik.setPopup(popup).togglePopup()
+    prik.setPopup(popup).togglePopup() // lav popuppet
     }
 
-    nyPrik()
+    nyPrik() // toggle funktionen
 
+
+    // Sorter lokationer funktion
     const sorterDestinationer = (locations) => {
-      const pointsForDestinations = locations.map((destination) => {
-        return convertToPoints(destination)
+      // lav adresser om til points af typen lng lat, som ses i funktionen : convertToPoints
+      const pointsForDestinations = locations.map((destination) => { // for hver adresse
+        return convertToPoints(destination) // lav adresse til convertToPoints (altså lng lat type)
       })
+
+      // call stuff
       const callParameters = {
-        key: "6r7urAO3rpFC6j9sPheFAv0NYswcxfaQ",
-        destinations: pointsForDestinations,
-        origins: [convertToPoints(origin)],
+        key: "6r7urAO3rpFC6j9sPheFAv0NYswcxfaQ", // vi bruger api nølgen til at få adgang til apien
+        destinations: pointsForDestinations, // vi har vores destinationer, som vi lige har lavet om til den rigtige type
+        origins: [convertToPoints(origin)], // og vi har vores start lokation
       }
+
+      // Så kan vi sortere destinationerne
       return new Promise((resolve, reject) => {
-        ttapi.services
-          .matrixRouting(callParameters)
+        ttapi.services // ved brug af api fra tomtom
+          .matrixRouting(callParameters) // Vi bruger matrixrouting, som modtager adresserne
           .then((matrixAPIResults) => {
-            const results = matrixAPIResults.matrix[0]
-            const resultsArray = results.map((result, index) => {
+            const results = matrixAPIResults.matrix[0] // vi tager resultatet af vores matrixrouting
+            const resultsArray = results.map((result, index) => { // for hver resultat skal vi:
               return {
-                location: locations[index],
-                drivingtime: result.response.routeSummary.travelTimeInSeconds,
+                location: locations[index], // lokationen
+                drivingtime: result.response.routeSummary.travelTimeInSeconds, // tid til at nå til lokationen
               }
             })
+
+            // Så sortere vi dem efter hvor længe det tager at køre.
             resultsArray.sort((a,b) => {
               return a.drivingtime - b.drivingtime
             })
-            const sortedLokationer = resultsArray.map((result) => {
+            // Så til sidst returnere vi resultat og lokationen af resultatet
+            const sortedLokationer = resultsArray.map((result) => { // for hver resultat
               return result.location
             })
             resolve(sortedLokationer)
@@ -132,6 +151,7 @@ const App = () => {
       })
     }
 
+    // Når flere adresser bliver tilføjet, beregner vi ruten om igen.
     const recalculateRuter = () => {
       sorterDestinationer(destinations).then((sorted) => {
         sorted.unshift(origin)
@@ -148,14 +168,15 @@ const App = () => {
       })
     }
 
+    // Når man trykker på mappet, og tilføjer en ny destination:
     map.on('click', (e) => {
-      destinations.push(e.lngLat)
+      destinations.push(e.lngLat) // tilføj destination
       nySkraldHentLokation(e.lngLat, map)
-      recalculateRuter()
+      recalculateRuter() // beregn ruterne igen, med den nye destination
     })
 
   }, [])
-  return (
+  return ( // Retuner et div, med refferance til mappet, og ellers bare teksten der står
     <div className="App">
       <div ref={mapElement} className="map">
         <h1>Smart Skraldebil Ruteplan</h1>
@@ -165,4 +186,3 @@ const App = () => {
 }
 
 export default App;
-// sidst video rute kom ik frem
